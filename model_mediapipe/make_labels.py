572c32f57defbb32ed84c8e7048dba7a298d2f25
milh0002 <142796331+milh0002@users.noreply.github.com>
2023-10-29 15:05:00 +0800

    Add files via upload

--- model_mediapipe/make_labels.py
+++ model_mediapipe/make_labels.py
@@ -6,6 +6,8 @@ import sys
 
 def make_seq_and_labels(label_map, DATA_PATH):
     sequences1, labels1 = [], []
+
+    # Loop through every file in every directory
     for action in os.listdir(DATA_PATH):
         fd = os.path.join(DATA_PATH + "/" + action)
         for sequence in os.listdir(fd):
@@ -13,8 +15,12 @@ def make_seq_and_labels(label_map, DATA_PATH):
             fd1 = os.path.join(DATA_PATH + "/" + action + "/" + sequence)
             for frame in os.listdir(fd1):
                 res = np.load(os.path.join(DATA_PATH + "/" + action + "/" + sequence + "/" + frame))
+
+                # Remove all inhomogeneous data
                 if len(np.array(res, dtype=object).shape) == 1:
                     window.append(res)
+
+            # Only accept 30 frames data
             if len(window) == 30:
                 sequences1.append(window)
                 labels1.append(label_map[action])
@@ -22,11 +28,16 @@ def make_seq_and_labels(label_map, DATA_PATH):
 
 
 if __name__ == "__main__":
+    # Insert the current directory into the sys path
     sys.path.insert(0, './')
+
+    # Define the data path and action labels
     PATH = os.path.join('DATA')
     actions = np.array(["Sitting", "Standing", "Walking"])
     label_m = {label: num for num, label in enumerate(actions)}
     sequences, labels = make_seq_and_labels(label_m, PATH)
+
+    # Convert sequences and labels to numpy arrays
     X = np.array(sequences)
     y = to_categorical(labels).astype(int)
 
