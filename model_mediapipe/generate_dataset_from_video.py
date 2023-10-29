f8fea13d57fa3ed58c534f553a558ac77de9e973
milh0002 <142796331+milh0002@users.noreply.github.com>
2023-10-29 15:02:07 +0800

    Add files via upload

--- model_mediapipe/generate_dataset_from_video.py
+++ model_mediapipe/generate_dataset_from_video.py
@@ -8,20 +8,28 @@ import tensorflow as tf
 import sys
 from mediapipe_pose import mediapipe_pose
 
+# Add the parent directory to the sys path for importing your own module (mediapipe_pose)
 sys.path.insert(0, '../')
 
-
+# Set the path for storing data
 DATA_PATH = "../DATA/"
 if not os.path.exists(DATA_PATH):
     os.mkdir(DATA_PATH)
 
+# Initialize the MediaPipe Pose model
 mp = mediapipe_pose()
 
+# Initialize time variables
 cTime, pTime = 0, 0
 
+# Define the sequence length for capturing frames
 sequence_length = 30
 path = "../test/"
+
+# List of actions based on directory names
 actions = os.listdir(path)
+
+# Iterate through actions and videos to capture frames
 for action in actions:
     if not os.path.exists(DATA_PATH + action):
         os.mkdir(DATA_PATH + action)
@@ -31,6 +39,8 @@ for action in actions:
     for video in range(len(video_list)):
         cd = os.path.join(path + action + "/" + video_list[video])
         cap = cv2.VideoCapture(cd)
+
+        # Initialize the MediaPipe Holistic model
         with mp.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
             if video < extra_sequences:
                 no_sequences += 1
@@ -45,11 +55,15 @@ for action in actions:
                 for frame_num in range(sequence_length):
                     ref, frame = cap.read()
                     try:
+                        # Perform MediaPipe pose detection on the frame
                         image, results = mp.mediapipe_detection(frame, holistic)
                     except:
                         break
 
+                    # Draw styled landmarks on the frame
                     mp.draw_styled_landmarks(image, results)
+
+                    # Calculate and display FPS
                     cTime = time.time()
                     fps = 1 / (cTime - pTime)
                     pTime = cTime
@@ -65,10 +79,13 @@ for action in actions:
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1, cv2.LINE_AA)
                         cv2.imshow('OpenCV Feed', image)
 
+                    # Extract key points and save as a .npy file
                     keypoints = mp.extract_keypoints(results)
                     npy_path = os.path.join(DATA_PATH, action, str(pre_sequences + sequence), str(frame_num))
                     np.save(npy_path, keypoints)
                     if cv2.waitKey(1) & 0xFF == ord("q"):
                         break
+
+# Release the video capture and close OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
